71e49ea40fe852b4dadfa46afd07c5f9299b9ea6
joel vilca Alania <joevil23tjq@gmail.com>
2019-06-11 18:27:19 -0500

    logo responsive

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
 
 import React, { Component } from 'react';
 import './App.css';
-import {Layout, Header ,Navigation,Drawer,Content} from 'react-mdl';
+import {Layout, Header ,Navigation,Drawer,Content, Menu, MenuItem} from 'react-mdl';
 import Nav from 'react-bootstrap/Nav'
 import Main from './main';
 import {Link} from 'react-router-dom';
 import Contact from './Contact';
+import logo from './image/logo.jpeg'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Nav as="ul" style={{backgroundColor: '#155799', color:'white',display: 'none'}}>
-  <Nav.Item as="li">
-    <Nav.Link  style={{ color:'white'}} href="/intranet">Intranet</Nav.Link>
-  </Nav.Item>
-  <Nav.Item as="li">
-    <Nav.Link  style={{ color:'white'}} href="/resoluciones">Resoluciones</Nav.Link>
-  </Nav.Item>
-  <Nav.Item as="li">
-    <Nav.Link  style={{ color:'white'}} href="/biblioteca">Biblioteca</Nav.Link>
-  </Nav.Item>
-  <Nav.Item as="li">
-    <Nav.Link  style={{ color:'white'}} href="/trabajo">Bolsa de Trabajo</Nav.Link>
-  </Nav.Item>
-  <Nav.Item as="li">
-    <Nav.Link  style={{ color:'white'}} href="/galeria">Galeria de fotos</Nav.Link>
-  </Nav.Item>
-</Nav>
+        <Nav as="ul" className="pri-nav justify-content-end" >
+          <Nav.Item as="li">
+            <Nav.Link  style={{ color:'white'}} href="/intranet">Intranet</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link  style={{ color:'white'}} href="/resoluciones">Resoluciones</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link  style={{ color:'white'}} href="/biblioteca">Biblioteca</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link  style={{ color:'white'}} href="/trabajo">Bolsa de Trabajo</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link  style={{ color:'white'}} href="/galeria">Galeria de fotos</Nav.Link>
+          </Nav.Item>
+        </Nav>
         <Layout>
           <Header className="header-color" title="UNIVERSIDAD" scroll>
               <Navigation>
                 <Link to="/Inicio">Inicio</Link>
-                <Link to="/Nosotros">Nosotros</Link>
+                <Link to="/Nosotros" id="demo-menu-lower-left" >
+                  Nosotros
+                <Menu target="demo-menu-lower-left">
+                  <MenuItem>Universidad</MenuItem>
+                  <MenuItem>Mision vision</MenuItem>
+                  <MenuItem>Autoridades</MenuItem>
+                  <MenuItem>Docentes</MenuItem>
+                </Menu>
+                </Link>
                 <Link to="/Interculturalidad">Interculturalidad</Link>
                 <Link to="/Escuelas">Escuelas</Link>
                 <Link to="/Investigación">Investigación</Link>
@@ -54,6 +63,9 @@ class App extends Component {
               </Navigation>
           </Drawer>
           <Content>
+              <div className="page-content">
+                <img src={logo}  alt="logo"  className="logo"/>
+              </div>
               <Main />
               <Contact></Contact>
           </Content>
--- src/main.js
+++ src/main.js
@@ -1,23 +1,11 @@
 import React, { Component } from 'react';
 import { Grid, Cell, Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
-import Contact from './Contact'
 import Slider from './Slider'
 class Main extends Component {
   render() {
     return(
         <div>
             <Slider></Slider> 
-            <Grid style={{background: 'url(https://i.pinimg.com/564x/c9/89/a0/c989a0dc7eb64c294e6628a663453f5f.jpg) center / cover', height:'400px'}}>
-                <h2>Boletin Informativo</h2>
-                <Cell col={12} className="banner-text">
-                <img 
-                    src="https://media.wired.com/photos/5b36a5b6e237e87ce9592b8e/master/pass/Github-Microsoft-BIZ-FINAL.jpg"
-                    alt="joel vilca"
-                    className="perfil-img"
-                />
-                <p>HTML/CSS | Javascript | React | React-Native | Redux | Php | Mysql | Laravel</p>
-                </Cell>
-            </Grid>
             <Grid style={{backgroundColor: 'white', height:'400px'}}>
                 <h2>ACTIVIDADES OFICIALES</h2>
                 <Card shadow={0} style={{width: '320px', height: '320px', marginTop: '5%'}}>
@@ -31,7 +19,17 @@ class Main extends Component {
                     </CardActions>
                 </Card>
             </Grid>
-            
+            <Grid style={{background: 'url(https://i.pinimg.com/564x/c9/89/a0/c989a0dc7eb64c294e6628a663453f5f.jpg) center / cover', height:'400px'}}>
+                <h2>Boletin Informativo</h2>
+                <Cell col={12} className="banner-text">
+                <img 
+                    src="https://media.wired.com/photos/5b36a5b6e237e87ce9592b8e/master/pass/Github-Microsoft-BIZ-FINAL.jpg"
+                    alt="joel vilca"
+                    className="perfil-img"
+                />
+                <p>HTML/CSS | Javascript | React | React-Native | Redux | Php | Mysql | Laravel</p>
+                </Cell>
+            </Grid>
         </div>
     )
   }
