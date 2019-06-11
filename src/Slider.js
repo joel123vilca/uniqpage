0219cfb9b56d3b3cee882cd3a2c93c07c3e8782d
joel vilca Alania <joevil23tjq@gmail.com>
2019-06-11 10:26:43 -0500

    slider

--- src/Slider.js
+++ src/Slider.js
@@ -1,17 +1,73 @@
-import React, { Component } from 'react';
-import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/styles.css';
+import React, { Component } from 'react'
+import  Carousel from 'react-bootstrap/Carousel'
 
 class Slider extends Component {
-    render(){
+    constructor(props, context) {
+      super(props, context);
+  
+      this.handleSelect = this.handleSelect.bind(this);
+  
+      this.state = {
+        index: 0,
+        direction: null,
+      };
+    }
+  
+    handleSelect(selectedIndex, e) {
+      this.setState({
+        index: selectedIndex,
+        direction: e.direction,
+      });
+    }
+  
+    render() {
+      const { index, direction } = this.state;
+  
       return (
-        <AwesomeSlider className="aws-btn">
-          <div data-src="http://uniq.edu.pe/public/img/imagenes/uniq/universidad.jpg" style={{height: '300px', position: 'relative'}}/>
-          <div data-src="http://uniq.edu.pe/public/img/imagenes/uniq/universidad.jpg" style={{height: '300px', position: 'relative'}} />
-          <div data-src="http://uniq.edu.pe/public/img/imagenes/uniq/universidad.jpg" style={{height: '300px', position: 'relative'}} />
-        </AwesomeSlider>
+        <Carousel
+          activeIndex={index}
+          direction={direction}
+          onSelect={this.handleSelect}
+        >
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://www.usm.cl/assets/img/carreras/fotos/vina-del-mar-tecnico-universitario-en-informatica.w700.jpg"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://www.usm.cl/assets/img/carreras/fotos/vina-del-mar-tecnico-universitario-en-informatica.w700.jpg"
+              alt="Third slide"
+            />
+  
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://www.usm.cl/assets/img/carreras/fotos/vina-del-mar-tecnico-universitario-en-informatica.w700.jpg"
+              alt="Third slide"
+            />
+  
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       );
     }
 }
-
 export default Slider;
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 import Contact from './Contact'
-import Slider from './Components/Main'
+import Slider from './Slider'
 class Main extends Component {
   render() {
     return(
         <div>
             <Slider></Slider> 
-        
+            <Contact></Contact>
         </div>
     )
   }
