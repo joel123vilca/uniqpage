e7ee3f35e9feca57413f5de1017491b10ad5f683
joel vilca Alania <joevil23tjq@gmail.com>
2019-06-12 12:09:21 -0500

    events design progress

--- src/Events.js
+++ src/Events.js
@@ -7,7 +7,7 @@ class Events extends Component{
             <div>
               <h2 style={{lineHeight: '1.25em', fontSize:'3.0517578125em', fontFamily:'Source Sans Pro,Helvetica Neue,Helvetica,Arial,sans-serif',fontWeight: '900',textAlign:'center'}}>BOLETIN INFORMATIVO</h2>
               <Grid style={{backgroundColor: 'white', height:'400px'}}>
-                <Card shadow={0} style={{width: '320px', height: '320px', marginTop: '5%'}}>
+                <Card shadow={0} style={{width: '320px', height: '320px', marginTop: '1%', marginLeft:'5%',padding:'10px'}}>
                     <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>
                         <span style={{height: '78px', width:'78px',padding:'10px', background: '#2c3e50',color: '#fff', fontSize: '24px', fontWeight: '500', textAlign:'center'}}>
                             Junio
@@ -20,11 +20,43 @@ class Events extends Component{
                         <time>through May 14, 2020</time>
                         </CardText>
                     <CardActions border>
-                        <Button colored>View Updates</Button>
+                        <Button  colored>View Updates</Button>
+                    </CardActions>
+                </Card>
+                <Card shadow={0} style={{width: '320px', height: '320px', marginTop: '1%', marginLeft:'5%',padding:'10px'}}>
+                    <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>
+                        <span style={{height: '78px', width:'78px',padding:'10px', background: '#2c3e50',color: '#fff', fontSize: '24px', fontWeight: '500', textAlign:'center'}}>
+                            Junio
+                            12
+                        </span>
+                    </CardTitle>
+                        <CardText style={{ color:'black'}}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Aenan convallis.<br/><br/>
+                        <time>through May 14, 2020</time>
+                        </CardText>
+                    <CardActions border>
+                        <Button  colored>View Updates</Button>
+                    </CardActions>
+                </Card>
+                <Card shadow={0} style={{width: '320px', height: '320px', marginTop: '1%', marginLeft:'5%',padding:'10px'}}>
+                    <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>
+                        <span style={{height: '78px', width:'78px',padding:'10px', background: '#2c3e50',color: '#fff', fontSize: '24px', fontWeight: '500', textAlign:'center'}}>
+                            Junio
+                            12
+                        </span>
+                    </CardTitle>
+                        <CardText style={{ color:'black'}}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Aenan convallis.<br/><br/>
+                        <time>through May 14, 2020</time>
+                        </CardText>
+                    <CardActions border>
+                        <Button  colored>View Updates</Button>
                     </CardActions>
                 </Card>
               </Grid>
-              <Button colored style={{marginTop:'20px'}}>Mas Eventos de la UNIQ</Button>
+              <Button colored style={{marginTop:'20px', textAlign:'center'}}>Mas Eventos de la UNIQ</Button>
             </div>
         );
     }
