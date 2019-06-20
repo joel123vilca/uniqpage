145268ce289ce5dfefb1f4fc690616503b2ede2b
joel vilca Alania <joevil23tjq@gmail.com>
2019-06-20 15:54:21 -0500

    dev bulletin

--- src/Components/Bulletin.js
+++ src/Components/Bulletin.js
@@ -1,43 +1,70 @@
 import React, { Component } from 'react';
+import { CardText, Card, CardTitle, CardActions, Button } from 'react-mdl'; 
 
 class Bulletin extends Component {
     render(){
         return (
             <section className="bulletin">
+                <div><h2>Boletin informativo</h2></div>
                 <div className="bulletin--content">
-                    <img 
-                         className="bulletin--image"
-                        src="http://image.prntscr.com/image/15f7d1b8dca94296b249f56eb6cc78d3.png"
-                        alt="image bulletin 1"
-                    />
+                <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
+                    <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>Update</CardTitle>
+                    <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Aenan convallis.
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>View Updates</Button>
+                    </CardActions>
+                </Card>
                 </div>
                 <div className="bulletin--content">
-                    <img 
-                         className="bulletin--image"
-                        src="http://image.prntscr.com/image/f2b0ac9e43334eddac9c1af05e573888.png"
-                        alt="image bulletin 2"
-                    />
+                <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
+                    <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>Update</CardTitle>
+                    <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Aenan convallis.
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>View Updates</Button>
+                    </CardActions>
+                </Card>
                 </div>
                 <div className="bulletin--content">
-                    <img 
-                         className="bulletin--image"
-                        src="http://image.prntscr.com/image/6915d39cf813481fa3c19fa292c582ba.png"
-                        alt="image bulletin 2"
-                    />
+                <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
+                    <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>Update</CardTitle>
+                    <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Aenan convallis.
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>View Updates</Button>
+                    </CardActions>
+                </Card>
                 </div>
                 <div className="bulletin--content">
-                    <img 
-                        className="bulletin--image"
-                        src="http://image.prntscr.com/image/ad357d428faf4e88ab3bdac78782b523.png"
-                        alt="image bulletin 2"
-                    />
+                <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
+                    <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>Update</CardTitle>
+                    <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Aenan convallis.
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>View Updates</Button>
+                    </CardActions>
+                </Card>
                 </div>
                 <div className="bulletin--content">
-                    <img 
-                        className="bulletin--image"
-                        src="http://image.prntscr.com/image/7e98362d62b2490c998fe1472dcb0601.png"
-                        alt="image bulletin 2"
-                    />
+                <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
+                    <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>Update</CardTitle>
+                    <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Aenan convallis.
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>View Updates</Button>
+                    </CardActions>
+                </Card>
                 </div>
             </section>
         );
--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ export const SHOW_BANNERS = 'SHOW_BANNERS'
 export function showBanners() {
     
     return (dispatch, getState) => {
-        axios.get('http://api.uniq.edu.pe/public/api/banners')
+        axios.get('http://api3.uniq.edu.pe/public/api/banners')
             .then((response) => {
                 dispatch( { type: SHOW_BANNERS, payload: response.data.data } ) 
             }) 
