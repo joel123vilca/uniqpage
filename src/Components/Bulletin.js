ab2acd2c01b56ce3e4a540742590c1ba934d451b
joel vilca Alania <joevil23tjq@gmail.com>
2019-06-22 11:58:06 -0500

    bulletin card programing

--- src/Components/Bulletin.js
+++ src/Components/Bulletin.js
@@ -5,75 +5,93 @@ class Bulletin extends Component {
     render(){
         return (
             <section className="bulletin">
-                <div><h2>Boletin informativo</h2></div>
                 <div className="bulletin--content">
-                <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto',}}>
-                    {/* <img src="http://www.getmdl.io/assets/demos/dog.png"  style={{position:'relative'}}/>
-                    <span style={{height: '78px', width:'78px',padding:'10px', background: '#2c3e50',color: '#fff', fontSize: '24px', fontWeight: '500', textAlign:'center', position:'relative'}}>
-                            Junio
-                            12
-                    </span> */}
-                    <div className="bulletin--image">
-                        <img src="http://www.getmdl.io/assets/demos/dog.png" style={{width:'90%'}}></img>
+                <Card shadow={0} style={{width: '300px', height: '320px'}}>
+                    <div className="bulletin--card">
+                        <img src="http://www.maluenda.es/public/noticias/99_cursos_informatica.png" className="bulletin--image"></img>
                         <span className="bulletin--fecha">
                             Junio
                             12
                         </span>
                     </div>
-                    <CardText>
+                    <CardText className="bulletin--detail">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Aenan convallis.
                     </CardText>
-                    <CardActions border>
-                        <Button colored>View Updates</Button>
+                    <CardActions border className="bulletin--action">
+                        <Button colored>Ver más</Button>
                     </CardActions>
                 </Card>
                 </div>
                 <div className="bulletin--content">
-                <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
-                    <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>Update</CardTitle>
-                    <CardText>
+                <Card shadow={0} style={{width: '300px', height: '320px'}}>
+                    <div className="bulletin--card">
+                        <img src="http://www.maluenda.es/public/noticias/99_cursos_informatica.png" className="bulletin--image"></img>
+                        <span className="bulletin--fecha">
+                            Junio
+                            12
+                        </span>
+                    </div>
+                    <CardText className="bulletin--detail">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Aenan convallis.
                     </CardText>
-                    <CardActions border>
-                        <Button colored>View Updates</Button>
+                    <CardActions border className="bulletin--action">
+                        <Button colored>Ver más</Button>
                     </CardActions>
                 </Card>
                 </div>
                 <div className="bulletin--content">
-                <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
-                    <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>Update</CardTitle>
-                    <CardText>
+                <Card shadow={0} style={{width: '300px', height: '320px'}}>
+                    <div className="bulletin--card">
+                        <img src="http://www.maluenda.es/public/noticias/99_cursos_informatica.png" className="bulletin--image"></img>
+                        <span className="bulletin--fecha">
+                            Junio
+                            12
+                        </span>
+                    </div>
+                    <CardText className="bulletin--detail">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Aenan convallis.
                     </CardText>
-                    <CardActions border>
-                        <Button colored>View Updates</Button>
+                    <CardActions border className="bulletin--action">
+                        <Button colored>Ver más</Button>
                     </CardActions>
                 </Card>
                 </div>
                 <div className="bulletin--content">
-                <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
-                    <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>Update</CardTitle>
-                    <CardText>
+                <Card shadow={0} style={{width: '300px', height: '320px'}}>
+                    <div className="bulletin--card">
+                        <img src="http://www.maluenda.es/public/noticias/99_cursos_informatica.png" className="bulletin--image"></img>
+                        <span className="bulletin--fecha">
+                            Junio
+                            12
+                        </span>
+                    </div>
+                    <CardText className="bulletin--detail">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Aenan convallis.
                     </CardText>
-                    <CardActions border>
-                        <Button colored>View Updates</Button>
+                    <CardActions border className="bulletin--action">
+                        <Button colored>Ver más</Button>
                     </CardActions>
                 </Card>
                 </div>
                 <div className="bulletin--content">
-                <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
-                    <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>Update</CardTitle>
-                    <CardText>
+                <Card shadow={0} style={{width: '300px', height: '320px'}}>
+                    <div className="bulletin--card">
+                        <img src="http://www.maluenda.es/public/noticias/99_cursos_informatica.png" className="bulletin--image"></img>
+                        <span className="bulletin--fecha">
+                            Junio
+                            12
+                        </span>
+                    </div>
+                    <CardText className="bulletin--detail">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Aenan convallis.
                     </CardText>
-                    <CardActions border>
-                        <Button colored>View Updates</Button>
+                    <CardActions border className="bulletin--action">
+                        <Button colored>Ver más</Button>
                     </CardActions>
                 </Card>
                 </div>
