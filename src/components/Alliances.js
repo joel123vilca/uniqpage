e3c203d2c3f76a1185cb4727f1efb3c4c59aefb8
joel vilca Alania <joevil23tjq@gmail.com>
2019-07-22 17:16:17 -0500

    alliances dev

--- src/components/Alliances.js
+++ src/components/Alliances.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
+import {ListItem, ListItemContent,ListItemAction, List, Button} from 'react-mdl';
 import alianzas from '../image/alianzas.jpg';
 class Alliances extends Component {
   render() {
     return(
       <div style={{width: '100%', margin: 'auto'}}>
       <img
-          className="d-block w-100 slider"
-          src={alianzas}
-          alt="banner alianzas"
-        />
+        className="d-block w-100 slider"
+        src={alianzas}
+        alt="banner alianzas"
+      />
+      <h2 className="bulletin-title">Convenios de la UNIQ con otras Instituciones</h2>
+      <List style={{width: '90%'}}>
+        <ListItem>
+          <img
+            src={alianzas}
+            style={{width: '30%'}}
+            alt="banner alianzas"
+          />
+          <ListItemContent> CONVENIO ESPECÍFICO DE COOPERACIÓN ENTRE LA UNIVERSIDAD NACIONAL DE SAN ANTONIO ABAD DEL CUSCO Y LA UNIVERSIDAD NACIONAL INTERCULTURAL DE QUILLABAMBA</ListItemContent>
+          <ListItemAction>
+            <Button raised colored ripple>Descargar</Button>
+          </ListItemAction>
+        </ListItem>
+        <ListItem>
+          <img
+            src={alianzas}
+            style={{width: '30%'}}
+            alt="banner alianzas"
+          />
+          <ListItemContent>MEMORANDUM DE ENTENDIMIENTO ENTRE LA UNIVERSIDAD NACIONAL INTERCULTURAL DE QUILLABAMBA, PERU Y LA UNIVERSIDAD DE MENDEL EN BRNO, REimgA CHECA (ESPAÑOL)</ListItemContent>
+          <ListItemAction>
+            <Button raised colored ripple>Descargar</Button>
+          </ListItemAction>
+        </ListItem>
+      </List>
+      <h2 className="bulletin-title">Red de Universidades Interculturales</h2>
+      <img
+        src={alianzas}
+        style={{width: '30%'}}
+        alt="banner alianzas"
+      />
+      <img
+        src={alianzas}
+        style={{width: '30%'}}
+        alt="banner alianzas"
+      />
+      <br></br>
     </div>
     )
   }
--- src/components/Mision.js
+++ src/components/Mision.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
+import {Card, CardText} from 'react-mdl';
 import mision from '../image/mision.jpg';
 class Mision extends Component {
   render() {
     return(
       <div style={{width: '100%', margin: 'auto'}}>
       <img
-          className="d-block w-100 slider"
-          src={mision}
-          alt="banner mision"
-        />
+        className="d-block w-100 slider"
+        src={mision}
+        alt="banner mision"
+      />
+      <Card shadow={0} style={{width: '100%', margin: 'auto'}}>
+        <h2 className="bulletin-title">Misión</h2>
+        <CardText style={{ textIndent: '50px', textAlign: 'justify',letterSpacing: '1px', alignItems:'center'}}>
+        "Los peruanos acceden a una educación que les permite desarrollar su potencial desde la primera infancia y convertirse en ciudadanos que valoran su cultura, conocen sus derechos y responsabilidades, desarrollan sus talentos y participan de manera innovadora, competitiva y comprometida en las dinámicas sociales, 
+        contribuyendo al desarrollo de sus comunidades y del país en su conjunto".
+        </CardText>
+        <CardText style={{ textIndent: '50px', textAlign: 'justify',letterSpacing: '1px', alignItems:'center'}}>
+        "Maganiro timaigatsirira aka peroku, ikogakoigaka isankevantavageigakera kametiniri iragaveaigakeniri irogovageiganakera, onti okogakotaka intsititagaiganakenkanira ityomiakyanira, impo irantaritanakera, irogotasanovageigakeniri, aikiro irogoigakerora ganiri ipegakoigaro yogameigarira, ganiri tyani verajairi, aikiro irogoigakera intaigakemparora irantaneegi, ariompari inkogasanoiganakeri irogotasanovageiganakera, aikiro irogogetanakerora tekyarira irogotumageigero maika, intentagaiganakemparira pashini govageigatsirira inkañoiganakemparira iriroegi, irogotagaiganakerira maganiro ishaninkaegi tyara inkantaigakempa itimayganakera, irogotagageiganakerira, tyara inkantaigakempa aiompaniri irogovageiganakeri 
+        paniropage itimageigira intimavageigakeniri kameti maganiro timaigatsirira aka kipatsiku" (Traductor. Angel Diaz Olivera)
+        </CardText>
+      </Card>
+      <Card shadow={0} style={{width: '100%', margin: 'auto'}}>
+        <h2 className="bulletin-title">Visión</h2>
+        <CardText style={{ textIndent: '50px', textAlign: 'justify',letterSpacing: '1px', alignItems:'center'}}>
+        "Brindar formación profesional universitaria de calidad al estudiante universitario, en base a la generación de conocimiento humanístico, cientifico, 
+        tecnológico y con enfoque intercultural, que contribuye al desarrollo sostenible de la sociedad".
+        </CardText>
+        <CardText style={{ textIndent: '50px', textAlign: 'justify',letterSpacing: '1px', alignItems:'center'}}>
+        "Intimaiganakeniri matsigenkaegi govageiganankitsinerira isankevantavageigakera ontimaigakerira irantaneegi, ontirari isankevantaigake anta pairorira avisake ogotagantagetunkanirira pairorira okogagetaka ogotakenkanira ontirari origotagaigakenkani tyara inkantaigakempa irogoiganakera intimavageigakera kameti, irogotasanovageiganakera, irogoaganogetakenkanira terira irogoigero maika, 
+        aikiro irogotagagetakenkanira irashiegi iriroegi yogamegeigarira, okari oka amutakoigakeriniri tyara inkantaigakempa."(Traductor. Angel Diaz Olivera)
+        </CardText>
+      </Card>
     </div>
     )
   }
