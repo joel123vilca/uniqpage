983550fedd68f3f6ae1ed274341afc0badbb546a
joel vilca Alania <joevil23tjq@gmail.com>
2019-09-16 17:29:29 -0500

    bulletin list dev

--- src/components/Bulletins.js
+++ src/components/Bulletins.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
-
+import { connect } from 'react-redux';
+import { showStories } from '../actions';
 class Bulletins extends Component {
+    componentWillMount(){
+        this.props.showStories()
+    }
+    renderListBulletin(){
+        return this.props.stories.map((storie) => {
+            return (
+                <div key={storie.id}>
+                    <h4>{storie.title}</h4>
+                    <div>
+                    <span>{storie.month}</span>
+                    <span>{storie.day}</span>
+                    </div>
+                </div>
+            )
+        });
+    }
+
     render(){
         return(
             <div>
                 <h3>Lista de Boletin Informativo UNIQ</h3>
+                {this.renderListBulletin()}
             </div>
         )
     }
 }
+function mapStateToProps(state) {
+    return {
+      stories: state.storie.lista
+    }
+}
 
-export default Bulletins;
+export default connect(mapStateToProps, { showStories })(Bulletins);
