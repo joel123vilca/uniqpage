d262cf14db82f3c106820fdf3f9f4e4bd18b03fb
joel vilca Alania <joevil23tjq@gmail.com>
2019-10-18 16:20:23 -0500

    change routes in directories

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -67,7 +67,7 @@ const Main = () => (
         <Route  path='/gestion-academica' component={ListDocuments} />
         <Route  path='/oci' component={Oci} />
         <Route  path='/abastecimientos' component={Quotations} />
-        <Route  path='/directorio-institucional' component={Directory} />
+        <Route  path='/directorioelectronico' component={Directory} />
         <Route  path="*" component={Notfound}/>
     </Switch>
 )
