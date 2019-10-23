6344a75f03b01b63c13e3e124e23bf9bc89cc1fd
joel vilca Alania <joevil23tjq@gmail.com>
2019-10-23 12:48:39 -0500

    desing of table for directory list

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
-
+import DirectoryList from './DirectoryList';
 class Directory extends Component {
     render(){
         return (
-            <center>
-              <embed src="http://test1.uniq.edu.pe/public/storage/directorio/directorio%202019%20actualizado%2003102019.pdf" width="80%" height="550" />
-            </center>
+            <div>
+                <center>
+                    <embed src="http://test1.uniq.edu.pe/public/storage/directorio/directorio%202019%20actualizado%2003102019.pdf" width="80%" height="550" />
+                    <DirectoryList />
+                </center>
+            </div>
         )
     }
 }
